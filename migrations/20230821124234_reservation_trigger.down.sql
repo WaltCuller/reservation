e620707cc2d2d43d02f223ea7a9e613d7ed5e6f0
WaltCuller <baoz1509253022@outlook.com>
2023-08-21 20:44:02 +0800

    feat: updates reservation_trigger SQL

---
+++ migrations/20230821124234_reservation_trigger.down.sql
@@ -0,0 +1,4 @@
+-- Add down migration script here
+DROP TRIGGER reservations_trigger ON rsvp.reservations;
+DROP FUNCTION rsvp.reservations_trigger();
+DROP TABLE rsvp.reservation_changes CASCADE;
---
+++ migrations/20230821124234_reservation_trigger.up.sql
@@ -0,0 +1,44 @@
+-- Add up migration script here
+
+-- resevation change queue
+CREATE TABLE rsvp.reservation_changes (
+                                          id SERIAL NOT NULL,
+                                          reservation_id BIGSERIAL NOT NULL,
+                                          old JSONB,
+                                          new JSONB,
+                                          op rsvp.reservation_update_type NOT NULL,
+                                          CONSTRAINT reservation_changes_pkey PRIMARY KEY (id)
+);
+CREATE INDEX reservation_changes_reservation_id_op_idx ON rsvp.reservation_changes (reservation_id, op);
+
+-- server read cursor
+CREATE TABLE rsvp.server_read_cursor (
+                                         server_id VARCHAR(64) NOT NULL,
+                                         last_change_id BIGSERIAL NOT NULL,
+                                         CONSTRAINT reservation_changes_cursor_pkey PRIMARY KEY (server_id)
+);
+
+-- trigger for add/update/delete a reservation
+CREATE OR REPLACE FUNCTION rsvp.reservations_trigger() RETURNS TRIGGER AS $$
+BEGIN
+    IF TG_OP = 'INSERT' THEN
+        -- update reservation_changes
+        INSERT INTO rsvp.reservation_changes (reservation_id, old, new, op) VALUES (NEW.id, null, to_jsonb(NEW), 'create');
+    ELSIF TG_OP = 'UPDATE' THEN
+        -- if status changed, update reservation_changes
+        IF OLD.status <> NEW.status THEN
+            INSERT INTO rsvp.reservation_changes (reservation_id, old, new, op) VALUES (NEW.id, to_jsonb(OLD), to_jsonb(NEW), 'update');
+END IF;
+    ELSIF TG_OP = 'DELETE' THEN
+        -- update reservation_changes
+        INSERT INTO rsvp.reservation_changes (reservation_id, old, new, op) VALUES (OLD.id, to_jsonb(OLD), null, 'delete');
+END IF;
+    -- notify a channel called reservation_update
+    NOTIFY reservation_update;
+RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER reservations_trigger
+    AFTER INSERT OR UPDATE OR DELETE ON rsvp.reservations
+    FOR EACH ROW EXECUTE PROCEDURE rsvp.reservations_trigger();
